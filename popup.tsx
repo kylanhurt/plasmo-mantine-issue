8a49bd9dfb71b54a06bce129b25464b86edc1b98
Kylan Hurt <kylan.hurt@gmail.com>
2024-10-06 16:46:01 -0500

    Reproduce error

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,16 @@
-import { Anchor, Button, Input, Stack, Text } from "@mantine/core"
-import { useState } from "react"
+import { Stack } from "@mantine/core"
 
 import "@mantine/core/styles.css"
+
 import { ThemeProvider } from "~theme"
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+import "./popup.scss"
 
+function IndexPopup() {
   return (
     <ThemeProvider>
       <Stack miw={240} p="lg">
-        <Text fw="bold" size="xl">
-          Welcome to your{" "}
-          <Anchor href="https://www.plasmo.com" target="_blank">
-            Plasmo
-          </Anchor>{" "}
-          Extension!
-        </Text>
-        <Input onChange={(e) => setData(e.target.value)} value={data} />
-        <Button component="a" href="https://docs.plasmo.com" target="_blank">
-          View Docs
-        </Button>
+        <span className="testing">Testing!</span>
       </Stack>
     </ThemeProvider>
   )
